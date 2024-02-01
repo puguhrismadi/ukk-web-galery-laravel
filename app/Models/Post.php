4628b308028a0ad8273202c32e8bfea37be794dc
Puguh Rismadi Ismail <puguh.rismadi@gmail.com>
2024-02-01 23:26:30 +0700

    add relation to model kategori

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Kategori;
 
 class Post extends Model
 {
@@ -16,5 +17,10 @@ class Post extends Model
         'user_id',
         'status'
     ];
+    //relasi 1 ke 1 
+    public function kategori()
+    {
+        return $this->belongsTo(Kategori::class);
+    }
 
 }
